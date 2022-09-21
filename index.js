65ca90dd235b428fb358395ce97f7f7c8245272f
Patrick <pcsong33@gmail.com>
2022-09-20 21:00:09 -0400

    fixed port error

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 // Connect to MongoDB
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
     console.log("Connected to db!");
-    app.listen(6000, () => console.log("Server Up and running"));
+    app.listen(3000, () => console.log("Server Up and running"));
 });
 
 // Set engine configuration
